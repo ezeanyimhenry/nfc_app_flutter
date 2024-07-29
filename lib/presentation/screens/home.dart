808028b2e3511fbb88f97d2e143b6ef7be7996b1
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-07-29 23:32:52 +0100

    refactored folder structure

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:nfc_app/presentation/screens/history_screen.dart';
-import 'package:nfc_app/presentation/screens/settings_screen.dart';
-import 'package:nfc_app/presentation/screens/write_nfc_screen.dart';
+import 'package:nfc_app/presentation/screens/history/history_screen.dart';
+import 'package:nfc_app/presentation/settings/settings_screen.dart';
+import 'package:nfc_app/presentation/screens/write-nfc/write_nfc_screen.dart';
 import 'package:nfc_app/presentation/widgets/app_bottom_nav.dart';
 import 'package:provider/provider.dart';
 
 import '../../notifier/bottom_nav.dart';
-import 'read_nfc_screen.dart';
+import 'read-nfc/read_nfc_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
