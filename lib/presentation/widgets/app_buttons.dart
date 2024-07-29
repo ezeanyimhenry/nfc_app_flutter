2435fc1d1851f048c248863421c6b47e904dbaba
Michael <michaelcee2000@gmail.com>
2024-07-29 01:29:49 +0100

    feat: app buttons

---
+++ lib/presentation/widgets/app_buttons.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_app/constants/app_colors.dart';
+import 'package:nfc_app/constants/app_textstyles.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final Function() onTap;
+  final String text;
+  final Color? color;
+  final Color? textColor;
+  final double? width;
+  final double? height;
+  const PrimaryButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+    this.width,
+    this.height,
+    this.color,
+    this.textColor,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onTap,
+      style: TextButton.styleFrom(
+        backgroundColor: color ?? AppColors.primaryColor,
+        minimumSize: Size(
+          width ?? double.infinity,
+          height ?? 48,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Text(
+        text,
+        style: AppTextStyle.primaryButtonText,
+      ),
+    );
+  }
+}
+
+// Secondary Button
+class SecondaryButton extends StatelessWidget {
+  final Function() onTap;
+  final String text;
+  final Color? color;
+  final Color? textColor;
+  final double? width;
+  final double? height;
+  const SecondaryButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+    this.width,
+    this.height,
+    this.color,
+    this.textColor,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onTap,
+      style: TextButton.styleFrom(
+        backgroundColor: color ?? AppColors.backgroundColor,
+        minimumSize: Size(
+          width ?? double.infinity,
+          height ?? 48,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        side: const BorderSide(width: 1, color: AppColors.secondaryColor),
+      ),
+      child: Text(
+        text,
+        style: AppTextStyle.secondaryButtonText,
+      ),
+    );
+  }
+}
