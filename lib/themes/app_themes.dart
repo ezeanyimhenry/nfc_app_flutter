a942e1e858be2b0369da8a8def098410b1e00110
Michael <michaelcee2000@gmail.com>
2024-07-29 01:28:04 +0100

    feat: app theme

---
+++ lib/themes/app_themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:nfc_app/constants/app_colors.dart';
+
+class AppThemeData {
+  AppThemeData._();
+
+  static final lightMode = ThemeData(
+      colorScheme: const ColorScheme.light(
+    surface: AppColors.backgroundColor,
+  ));
+}
