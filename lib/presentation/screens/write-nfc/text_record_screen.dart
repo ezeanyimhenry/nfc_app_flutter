fca16bf10911dec688b8384c95a6c4366e8f1acc
Michael <michaelcee2000@gmail.com>
2024-07-31 22:45:57 +0100

    fix: text-record

--- lib/presentation/screens/write-nfc/text_record_screen.dart
+++ lib/presentation/screens/write-nfc/text_record_screen.dart
@@ -50,139 +50,148 @@ class _TextRecordScreenState extends State<TextRecordScreen> {
 
   @override
   Widget build(BuildContext context) {
+    void removeFocus() {
+      FocusScope.of(context).unfocus();
+    }
+
     return ChangeNotifierProvider(
       create: (context) => NFCNotifier(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.backgroundColor2,
-          leading: IconButton(
-            onPressed: () => Navigator.of(context).pop(),
-            icon: SvgPicture.asset(
-              'assets/arrow_left.svg',
-              fit: BoxFit.scaleDown,
+      child: GestureDetector(
+        onTap: removeFocus,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.backgroundColor2,
+            leading: IconButton(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: SvgPicture.asset(
+                'assets/arrow_left.svg',
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+            title: Text(
+              'Text Record',
+              style: GoogleFonts.inter(
+                color: AppColors.primaryTextColor,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+              ),
             ),
           ),
-          title: Text(
-            'Text Record',
-            style: GoogleFonts.inter(
-              color: AppColors.primaryTextColor,
-              fontSize: 20.0,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-        body: Padding(
-          padding: AllPadding.padding16,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    'Enter Text Record',
-                    style: GoogleFonts.inter(
-                      color: AppColors.primaryTextColor,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
+          body: Padding(
+            padding: AllPadding.padding16,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Enter Text Record',
+                      style: GoogleFonts.inter(
+                        color: AppColors.primaryTextColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                  const YGap(value: 16.0),
-                  SizedBox(
-                    height: 121.0,
-                    child: TextField(
-                      controller: controller,
-                      maxLines: 4, // Allow up to 3 lines of text
-                      // minLines: 4, // Ensure at least one line is visible
-                      decoration: const InputDecoration(
-                        alignLabelWithHint: true,
-                        labelText: 'Enter text here',
-                        // labelStyle: ,
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: AppColors.primaryColor),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(8.0),
+                    const YGap(value: 16.0),
+                    SizedBox(
+                      height: 350,
+                      child: TextField(
+                        controller: controller,
+                        maxLines: 12, // Allow up to 3 lines of text
+                        // minLines: 4, // Ensure at least one line is visible
+                        decoration: const InputDecoration(
+                          alignLabelWithHint: true,
+                          labelText: 'Enter text here',
+                          // labelStyle: ,
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: AppColors.primaryColor),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
                           ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: AppColors.primaryColor,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.primaryColor,
+                            ),
                           ),
-                        ),
-                        constraints: BoxConstraints(
-                          maxHeight: 121,
+                          constraints: BoxConstraints(
+                            maxHeight: 121,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const YGap(value: 16.0),
-              PrimaryButton(
-                  onTap: () async {
-                    writeToNfc();
-                    //save to history
-                    List<HistoryModel> loadedHistoryList =
-                        await AppSharedPreference().getHistoryList();
-                    loadedHistoryList.add(HistoryModel(
-                        language: "English",
-                        date: DateTime.now(),
-                        actualText: controller.text,
-                        type: HistoryType.written));
-                    await AppSharedPreference()
-                        .saveHistoryList(loadedHistoryList);
-                  },
-                  text: 'Add'),
-              Consumer<NFCNotifier>(builder: (context, provider, _) {
-                if (provider.isProcessing) {
-                  // return const CircularProgressIndicator();
-                  showModalBottomSheet(
-                      isDismissible: false,
-                      context: context,
-                      builder: (context) {
-                        return SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.4,
-                          child: const AppBottomsheet(
-                            message: "Make sure your device is well placed.",
-                            title: "Scanning...",
-                            centerContent: ProgressIndicatorWithText(
-                              progress: 0.75,
-                            ),
-                          ),
-                        );
-                      });
-                }
-                if (provider.message.isNotEmpty) {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    Navigator.pop(context);
+                  ],
+                ),
+                const YGap(value: 16.0),
+                PrimaryButton(
+                    onTap: () async {
+                      writeToNfc();
+                      //save to history
+                      List<HistoryModel> loadedHistoryList =
+                          await AppSharedPreference().getHistoryList();
+                      loadedHistoryList.add(HistoryModel(
+                          language: "English",
+                          date: DateTime.now(),
+                          actualText: controller.text,
+                          type: HistoryType.written));
+                      await AppSharedPreference()
+                          .saveHistoryList(loadedHistoryList);
+                    },
+                    text: 'Add'),
+                Consumer<NFCNotifier>(builder: (context, provider, _) {
+                  if (provider.isProcessing) {
+                    // return const CircularProgressIndicator();
                     showModalBottomSheet(
                         isDismissible: false,
                         context: context,
                         builder: (context) {
                           return SizedBox(
                             height: MediaQuery.of(context).size.height * 0.4,
-                            child: AppBottomsheet(
-                              hasPrimaryButton: true,
-                              primaryButtonText: "Continue",
-                              primaryButtonOnTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (txt) => TranslateScreen(
-                                            message: provider.message)));
-                              },
+                            child: const AppBottomsheet(
                               message: "Make sure your device is well placed.",
-                              title: "Scan Successful!",
-                              centerContent: const ProgressIndicatorWithText(
-                                progress: 1.0,
+                              title: "Scanning...",
+                              centerContent: ProgressIndicatorWithText(
+                                progress: 0.75,
                               ),
                             ),
                           );
                         });
-                  });
-                }
-                return const SizedBox();
-              }),
-            ],
+                  }
+                  if (provider.message.isNotEmpty) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      Navigator.pop(context);
+                      showModalBottomSheet(
+                          isDismissible: false,
+                          context: context,
+                          builder: (context) {
+                            return SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              child: AppBottomsheet(
+                                hasPrimaryButton: true,
+                                primaryButtonText: "Continue",
+                                primaryButtonOnTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (txt) => TranslateScreen(
+                                              message: provider.message)));
+                                },
+                                message:
+                                    "Make sure your device is well placed.",
+                                title: "Scan Successful!",
+                                centerContent: const ProgressIndicatorWithText(
+                                  progress: 1.0,
+                                ),
+                              ),
+                            );
+                          });
+                    });
+                  }
+                  return const SizedBox();
+                }),
+              ],
+            ),
           ),
         ),
       ),
