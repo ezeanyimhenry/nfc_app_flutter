27a289434a5cd8746365a7c56a13b9fd14ff0a83
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-07-31 15:24:08 +0100

    fix

--- lib/presentation/screens/write-nfc/write_nfc_screen.dart
+++ lib/presentation/screens/write-nfc/write_nfc_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nfc_app/constants/app_colors.dart';
 import 'package:nfc_app/constants/app_spacing.dart';
